9394bd748a83b09a33c63c5c51ad0202bc5599d2
dialapus <dialapus@gmail.com>
2026-03-12 02:13:23 +0300

    feat: assemble dashboard layout with all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import Header from "./components/Header";
+import KpiRow from "./components/KpiRow";
+import ChatList from "./components/ChatList";
+import OrderList from "./components/OrderList";
+import TopListings from "./components/TopListings";
+import ActivityFeed from "./components/ActivityFeed";
+import { useAutoRefresh } from "./hooks/useAutoRefresh";
+
 export default function App() {
-  return <div className="p-8 text-center text-2xl">Avito Dashboard</div>;
+  const { data, prevData, lastUpdate, secondsLeft, isOnline } = useAutoRefresh();
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header lastUpdate={lastUpdate} secondsLeft={secondsLeft} isOnline={isOnline} />
+      <KpiRow data={data} prevData={prevData} />
+      <div className="grid grid-cols-2 gap-4 px-6 pb-6 flex-1">
+        <ChatList chats={data?.chats} />
+        <OrderList orders={data?.orders} />
+        <TopListings items={data?.items} />
+        <ActivityFeed data={data} />
+      </div>
+    </div>
+  );
 }
